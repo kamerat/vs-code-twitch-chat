d073d3101e03d1713135cf2837d393dd0211a58e
BlitZz96 <solve@rocketstudios.no>
2019-01-13 23:27:29 +0100

    Fix messages cache and settings ++
    I know.. Terrible commit message. I'm tired.

--- extension.js
+++ extension.js
@@ -1,5 +1,7 @@
 //TODO: oauth auto added to settings: https://stackoverflow.com/questions/49467421/updating-vs-code-user-settings-via-an-extension
-
+//TODO: add emoticons: https://www.npmjs.com/package/twitch-emoticons
+//TODO: Add user actions (ban, timeout, etc)
+//TODO: Add viewer counter?
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
@@ -12,14 +14,14 @@ const TwitchBot = require('twitch-bot')
 
 let messages = [{ 'markup': '<p>Welcome to the chat room!</p>' }]
 
-for (let index = 0; index < 100; index++) {
-	messages.push({
-		'markup': `
-		<div class="message">
-			<p><span style="color:#8a2be2;">the_blitzz</span>: Hello, this is a message</p>
-		</div>
-	` })
-}
+// for (let index = 0; index < 100; index++) {
+// 	messages.push({
+// 		'markup': `
+// 		<div class="message">
+// 			<p><span style="color:#8a2be2;">the_blitzz</span>: Hello, this is a message</p>
+// 		</div>
+// 	` })
+// }
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -33,28 +35,20 @@ function activate(context) {
 	let Bot = undefined
 	let unread = 0
 	
-	console.log('Congratulations, your extension "twitch-chat" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('twitch.open', function () {
 		const columnToShowIn = vscode.window.activeTextEditor
 			? vscode.window.activeTextEditor.viewColumn
 			: undefined;
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		//vscode.window.showInformationMessage('Hello World!');
 
 		//Instead of creating another panel on command, reveal tab
 		if (panel !== undefined) {
 			panel.reveal(columnToShowIn);
 		} else {
 			panel = vscode.window.createWebviewPanel(
-				'twitchChat', // Identifies the type of the webview. Used internally
-				'Twitch chat', // Title of the panel displayed to the user
-				columnToShowIn, // Editor column to show the new webview panel in.
+				'twitchChat',
+				'Twitch chat',
+				columnToShowIn,
 				{ 
 					enableScripts: true
 				}
@@ -62,7 +56,6 @@ function activate(context) {
 
 			// Init panel DOM 
 			panel.webview.html = getWebviewContent(messages.map(m => m.markup).join(''));
-
 
 			Bot = new TwitchBot({
 				username,
@@ -79,9 +72,11 @@ function activate(context) {
 				if (!panel.visible) {
 					unread++
 					panel.title = `(${unread}) Twitch chat`;
-					vscode.window.showInformationMessage(`${chatter.display_name}: ${chatter.message}`).then(selection => {
-						console.log(selection);
-					});
+					
+					// Display popup on new messages
+					if (config.alert) {
+						vscode.window.showInformationMessage(`${chatter.display_name}: ${chatter.message}`)
+					}
 				}
 
 				pushMessage(chatter, panel)
@@ -97,7 +92,6 @@ function activate(context) {
 			vscode.window.showErrorMessage(err.message);
 		})
 		
-
 		Bot.on('close', () => {
 			console.log("closed twitch chat connection");
 		})
@@ -106,10 +100,10 @@ function activate(context) {
 		panel.webview.onDidReceiveMessage(message => {
 			switch (message.command) {
 					case 'submit-message':
-					Bot.say(message.text, undefined, err => {
-						console.log(err);
-						vscode.window.showErrorMessage(err.message);
-					})
+						Bot.say(message.text, undefined, err => {
+							console.log(err);
+							vscode.window.showErrorMessage(err.message);
+						})
 						//Add our own message to cache because it does not trigger bot@message event
 						pushMessage({'message': message.text, 'display_name': username}, panel)
 						return;
@@ -119,11 +113,14 @@ function activate(context) {
 			context.subscriptions
 		);
 
-		panel.onDidChangeViewState((e) => {
+		panel.onDidChangeViewState(() => {
 			//Make scroll-bar goto bottom - so we are always at last message
 			panel.webview.postMessage({ command: 'resize' });
 
 			if (panel.visible) {
+				// Populate chat with cached messages
+				panel.webview.html = getWebviewContent(messages.map(m => m.markup).join(''));
+
 				//Remove notifications from title when user activates panel
 				setTimeout(() => {
 					panel.title = "Twitch chat";
@@ -133,15 +130,13 @@ function activate(context) {
 
 		panel.onDidDispose(() => {
 				// When the panel is closed, cancel any future updates to the webview content
-				//clearInterval(interval);
-				panel = undefined; //Reset panel when closed
+				panel = undefined; //Reset panel
 				Bot.close()
 			},
 			null,
 			context.subscriptions
 		);
 	});
-
 
 	context.subscriptions.push(disposable);
 }
@@ -212,6 +207,10 @@ function getWebviewContent(messages) {
 				window.addEventListener('message', event => {
 					const message = event.data;
 					switch (message.command) {
+						case 'message':
+							chat.insertAdjacentHTML('beforeend', message.markup);
+							chat.scrollTop = chat.scrollHeight;
+							break;
 						case 'resize':
 							chat.scrollTop = chat.scrollHeight;
 							break;
@@ -227,7 +226,7 @@ function getWebviewContent(messages) {
 								command: 'submit-message',
 								text: value
 							})
-							value = ''
+							event.target.value = ''
 						} else if (event.shiftKey) {
 							return true
 						}
@@ -240,17 +239,16 @@ function getWebviewContent(messages) {
 }
 
 function pushMessage(msg, panel) {
-
 	//Create new key that contains our markup for displaying the message.
 	msg.markup = `
 		<div class="message">
-			<p><span style="color:${msg.color}">${msg.display_name}</span>: ${msg.message}</p>
+			<p><span style="color:${msg.color ? msg.color : ''}">${msg.display_name}</span>: ${msg.message}</p>
 		</div>
 	`
 	//Push to our list of already cached session messages 
 	messages.push(msg)
 
-	panel.webview.html = getWebviewContent(messages.map(m => m.markup).join(''));
+	panel.webview.postMessage({ command: 'message', markup: msg.markup });
 }
 
 
